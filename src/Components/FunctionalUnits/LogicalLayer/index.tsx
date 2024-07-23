119d515b2a47c245c8d2cfab44caab87ce9f202e
Chris Stead <cmstead@Chriss-MBP.attlocal.net>
2024-07-23 09:14:29 -0700

    Added unit insert and vertical count update

--- src/Components/FunctionalUnits/LogicalLayer/index.tsx
+++ src/Components/FunctionalUnits/LogicalLayer/index.tsx
@@ -3,9 +3,16 @@ import LogicalUnit from "../LogicalUnit/index.tsx";
 
 export default function LogicalLayer() {
     const [legendValue, updateLegendValue] = useState("Logical Layer");
+    const [unitId, updateUnitId] = useState(1);
+    const [logicalUnits, updateLogicalUnits] = useState([{ id: 0 }]);
 
     function updateLegend({ target }) {
         updateLegendValue(target.value)
+    }
+
+    function addLogicalUnit() {
+        updateLogicalUnits(logicalUnits.concat({ id: unitId }));
+        updateUnitId(unitId + 1)
     }
 
     return (
@@ -14,7 +21,10 @@ export default function LogicalLayer() {
                 value={legendValue}
                 onChange={updateLegend}
             ></input></legend>
-            <LogicalUnit></LogicalUnit>
+            {logicalUnits.map(value => 
+                <LogicalUnit id={`logical-unit-${value.id}`} key={value.id}></LogicalUnit>
+            )}
+            <div><button onClick={addLogicalUnit}>Insert Unit</button></div>
         </fieldset>
     );
 }
--- src/Components/FunctionalUnits/index.tsx
+++ src/Components/FunctionalUnits/index.tsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import LogicalLayer from "./LogicalLayer/index.tsx";
+import { useDispatch } from "react-redux";
+import { actionTypes } from "../../services/constants.ts";
+
 
 export default function FunctionalUnits() {
     const [layerId, updateLayerId] = useState(1);
     const [layers, updateLayers] = useState([{ id: 0 }]);
+    const dispatch = useDispatch();
 
+    dispatch({
+        type: actionTypes.LAYER_COUNT_UPDATED,
+        payload: {
+            name: 'verticalCount',
+            value: layers.length
+        }
+    })
+    
     function insertLayer() {
         updateLayers(layers.concat({id: layerId}));
         updateLayerId(layerId + 1)
