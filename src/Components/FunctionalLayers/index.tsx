b89b56ffd5b178dbaea3d535fa3f1b0434e80d35
Chris Stead <cmstead@Chriss-MBP.attlocal.net>
2024-07-24 10:04:14 -0700

    Removed unused function declaration

--- src/Components/FunctionalLayers/index.tsx
+++ src/Components/FunctionalLayers/index.tsx
@@ -1,17 +1,17 @@
 import React, { ReactElement } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { horizontalCountSelector, verticalCountSelector } from "./functionalLayerCountSelectors.ts"
-import { actionTypes } from "../../services/constants.ts"
+// import { actionTypes } from "../../services/constants.ts"
 
-function computeValue(baseValue) {
-    if (baseValue === '') {
-        return baseValue
-    } else if (baseValue > 1) {
-        return Math.round(baseValue)
-    } else {
-        return 1
-    }
-}
+// function computeValue(baseValue) {
+//     if (baseValue === '') {
+//         return baseValue
+//     } else if (baseValue > 1) {
+//         return Math.round(baseValue)
+//     } else {
+//         return 1
+//     }
+// }
 
 export default function FunctionalLayers(): ReactElement {
 
@@ -20,17 +20,17 @@ export default function FunctionalLayers(): ReactElement {
 
     const dispatch = useDispatch()
 
-    const updateCount = ({ target }) => {
-        const baseValue = target.value
+    // const updateCount = ({ target }) => {
+    //     const baseValue = target.value
         
-        return dispatch({
-            type: actionTypes.LAYER_COUNT_UPDATED,
-            payload: {
-                name: target.name,
-                value: computeValue(baseValue)
-            }
-        })
-    }
+    //     return dispatch({
+    //         type: actionTypes.LAYER_COUNT_UPDATED,
+    //         payload: {
+    //             name: target.name,
+    //             value: computeValue(baseValue)
+    //         }
+    //     })
+    // }
 
     return (
         <ul>
